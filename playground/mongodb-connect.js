c8a5ef1d17c23db2a54f1f5500aa5095541a211f
Chen Yu,Xiang <peterleo1202@gmail.com>
2018-08-05 23:57:01 +0800

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,38 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    // db.collection('Users').insertOne({
+    //     name: 'fish',
+    //     age: 26,
+    //     location: 'Taipei'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    db.collection('Users').find({name: 'fish2'}).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('找無', err);
+    });
+
+    client.close();
+});
