2bea3a7b59bf37e65630c38d4a191f8cb0703513
Chen Yu,Xiang <peterleo1202@gmail.com>
2018-08-06 17:27:55 +0800

    mongodb crud

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -38,6 +38,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     //     console.log(result);
     // });
 
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5b66d6477dbb6e8402c05774')
+    }, {
+        $set: {name: 'fishwow'},
+        $inc: {age: +3}
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    })
 
     client.close();
 });
