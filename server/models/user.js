8cc5d7184758eee516c2b8b5aa70b6cf7ed64081
Chen Yu,Xiang <peterleo1202@gmail.com>
2018-08-06 23:42:07 +0800

    Test POST /todo route

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Users = mongoose.model('Users', {
     email: {
         type: String,
-        require: true,
+        required: true,
         trim: true,
         minlength: 1
     }
--- server/server.js
+++ server/server.js
@@ -25,3 +25,5 @@ app.post('/todo', (req, res) => {
 app.listen(3000, () => {
     console.log('Start port 3000');
 });
+
+module.exports = {app};
